fdb02cb0ab0d876d4c6d2bd6f26b5140fc2e1b34
anton <voevodin_anton_2000@mail.ru>
2022-04-08 21:33:20 +0400

    first commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/VoevodinAnton/vk-pdr-start-bot
+
+go 1.17
+
+require (
+	github.com/SevereCloud/vksdk/v2 v2.13.1
+	github.com/robfig/cron/v3 v3.0.1
+)
+
+require (
+	github.com/klauspost/compress v1.15.1 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/SevereCloud/vksdk/v2/api"
+	"github.com/SevereCloud/vksdk/v2/events"
+	longpoll "github.com/SevereCloud/vksdk/v2/longpoll-bot"
+	cron "github.com/robfig/cron/v3"
+)
+
+var VKToken = "890b57a42c1d670f6969f1336cf931c5d1dd6e3ad46538d0be86761544976acfc7a3679bf50cf483be251"
+
+func main() {
+	vk := api.NewVK(VKToken)
+
+	jakartaTime, _ := time.LoadLocation("Asia/Jakarta")
+	scheduler := cron.New(cron.WithLocation(jakartaTime))
+
+	lp, err := longpoll.NewLongPoll(vk, 212384138)
+	if err != nil {
+		panic(err)
+	}
+
+	lp.MessageNew(func(ctx context.Context, obj events.MessageNewObject) {
+		println(obj.Message.Text)
+		PeerId := obj.Message.PeerID
+		//println(PeerId)
+		if strings.Split(obj.Message.Text, " ")[1] == "start" {
+			log.Print("Job is started")
+			defer scheduler.Stop()
+			scheduler.AddFunc("* * * * *", func() { startPidorBot(vk, PeerId) })
+			go scheduler.Start()
+		}
+	})
+	lp.Run()
+}
+
+func startPidorBot(vk *api.VK, PeerId int) {
+	_, err := vk.MessagesSend(api.Params{"peer_id": PeerId, "random_id": 0, "message": "@botv_pidor старт"})
+	if err != nil {
+		log.Fatal("Error sending message:", err)
+	}
+	log.Print("Bot is started")
+}
